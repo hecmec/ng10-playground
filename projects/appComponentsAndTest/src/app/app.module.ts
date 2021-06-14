327de24f130cea095816469125a8f47a5d03f863
jenpet <jens-petzold@asys.fr>
2021-06-14 17:19:52 +0200

    added types

--- projects/appComponentsAndTest/src/app/app.module.ts
+++ projects/appComponentsAndTest/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, ModuleWithProviders } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { from } from 'rxjs';
@@ -61,6 +62,7 @@ const myProviders = [];
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    RouterModule,
     AppRoutingModule,
     MatButtonModule,
     MatInputModule,
