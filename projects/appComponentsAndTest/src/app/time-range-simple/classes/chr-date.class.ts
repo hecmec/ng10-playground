5996d09c69680dbb1b50bb96a3551a0247e81f26
jenpet <jens-petzold@asys.fr>
2021-08-13 15:25:21 +0200

    changed ts-list stuff

--- projects/appComponentsAndTest/src/app/time-range-simple/classes/chr-date.class.ts
+++ projects/appComponentsAndTest/src/app/time-range-simple/classes/chr-date.class.ts
@@ -29,18 +29,146 @@ export class ChrDate {
     return this._day;
   }
 
+  /*******************************************
+   * Static methods
+   *******************************************/
+
   /**
-   * gets a standard js date object with time to zero.
-   * Attention: this will have your local timezone and might not give the intended iso date. (iso is GMT)
+   * Parse string from iso date String 'yyyy-mm-dd'
+   * @param isoDateString: some
+   * @returns ChrDate object. Is null if creation impossbile
    */
-  public getStandardJsDate(): Date {
-    return new Date(this.year, this.month - 1, this.day);
+  public static createFromIsoString(isoDateString: string): ChrDate {
+    let date: ChrDate = null;
+    if (isoDateString) {
+      try {
+        const jsDate = new Date(isoDateString);
+        date = ChrDate.createFromDayMonthYear(jsDate.getDate(), jsDate.getMonth() + 1, jsDate.getFullYear());
+      } catch (err) {
+        // FIXME log to logger when dispo
+        console.error(err);
+        date = null;
+      }
+    }
+    return date;
   }
+
+  /**
+   * Parse string from french date String 'dd/mm/yyyy'
+   * @param isoDateString: iso
+   */
+  public static createFromFrenchDateString(dateString: string): ChrDate {
+    let date: ChrDate = null;
+    if (dateString) {
+      try {
+        dayjs.locale('fr');
+        const parseStrict = true;
+        const djsDate = dayjs(dateString, ['DD/MM/YYYY', 'D/M/YYYY', 'D/M/YY'], 'fr', parseStrict);
+        if (djsDate.isValid()) {
+          date = ChrDate.createFromDayMonthYear(djsDate.date(), djsDate.month() + 1, djsDate.year());
+        }
+      } catch (err) {
+        // FIXME log to logger when dispo
+        console.error(err);
+        date = null;
+      }
+    }
+    return date;
+  }
+
+  /**
+   * This creates only valid dates
+   * @param year number of the year
+   * @param month number of month (1 based not 0 like in js)
+   * @param day number of day
+   */
+  public static createFromDayMonthYear(day: number, month: number, year: number): ChrDate {
+    let date: ChrDate = null;
+    if (ChrDate._yearIsValid(year) && ChrDate._monthIsValid(month) && ChrDate._dayIsValid(day, month, year)) {
+      date = new ChrDate(day, month, year);
+    }
+    return date;
+  }
+
+  /**
+   * compares two dates
+   * @param aDate: date
+   * @param bDate: date
+   * @returns: negative if first is smaller
+   */
+  public static compare(aDate: ChrDate, bDate: ChrDate): number {
+    let result: number = 0;
+
+    if (aDate && bDate) {
+      const aDateIso = aDate.toIsoDateString();
+      const bDateIso = bDate.toIsoDateString();
+      if (aDateIso < bDateIso) {
+        result = -1;
+      } else if (aDateIso === bDateIso) {
+        result = 0;
+      } else {
+        result = 1;
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Gets the number of days in a month, with leap years, from the date object.
+   * @param year: 0 < year <= 9999
+   * @param month: number,  1 is january, 12 is december
+   */
+  private static _getMaxDaysOfMonthInYear(month: number, year: number): number {
+    let result = null;
+    if (year && year <= 9999 && month && month <= 12) {
+      try {
+        // this is interpreted as the last day of the previous month
+        // since months are 0 based
+        const date = new Date(year, month, 0);
+        result = date.getDate();
+      } catch (err) {
+        // FIXME log to logger when dispo
+        console.error(err);
+        result = null;
+      }
+    }
+
+    return result;
+  }
+
+  /**
+   * Years must be in the interval [minYear, maxYear]
+   * Validation function as pure function
+   * @param year: the year of the date
+   */
+  private static _yearIsValid(year: number): boolean {
+    return Times.minYear <= year && year <= Times.maxYear;
+  }
+
+  /**
+   * Month is valid, if it is in the interval [0, monthsInYear]
+   * @param month: month
+   */
+  private static _monthIsValid(month: number): boolean {
+    return 1 <= month && month <= Times.monthsInYear;
+  }
+
+  /**
+   * TODO: comment
+   * @param day: the day of the date 0 to 31
+   * @param month: the month of the date
+   * @param year: year of the date
+   */
+  private static _dayIsValid(day: number, month: number, year: number): boolean {
+    const maxDays = ChrDate._getMaxDaysOfMonthInYear(month, year);
+    return 0 < day && day <= maxDays;
+  }
+
   /**
    * Creates a ChrTime object
-   * @param year
-   * @param month
-   * @param day
+   * @param year:
+   * @param month:
+   * @param day:
    */
   protected constructor(day: number, month: number, year: number) {
     year = Math.abs(year);
@@ -63,24 +191,35 @@ export class ChrDate {
     //Object.freeze(this);
   }
 
+  /**
+   * gets a standard js date object with time to zero.
+   * Attention: this will have your local timezone and might not give the intended iso date. (iso is GMT)
+   */
+  public getStandardJsDate(): Date {
+    return new Date(this.year, this.month - 1, this.day);
+  }
+
   public isBefore(otherDate: ChrDate): boolean {
     let result = false;
     if (otherDate) {
-      return ChrDate.compare(this, otherDate) < 0;
+      result = ChrDate.compare(this, otherDate) < 0;
     }
+    return result;
   }
 
   public isSameOrBefore(otherDate: ChrDate): boolean {
     let result = false;
     if (otherDate) {
-      return ChrDate.compare(this, otherDate) <= 0;
+      result = ChrDate.compare(this, otherDate) <= 0;
     }
+    return result;
   }
   public isSame(otherDate: ChrDate): boolean {
     let result = false;
     if (otherDate) {
-      return ChrDate.compare(this, otherDate) === 0;
+      result = ChrDate.compare(this, otherDate) === 0;
     }
+    return result;
   }
   public isAfter(otherDate: ChrDate): boolean {
     throw new Error('Not yet implemented ...');
@@ -102,7 +241,7 @@ export class ChrDate {
 
   /**
    * Returns a french date string in the format dd/mm/yyyy.
-   * @param format
+   * @param format: slkdfj q
    */
   public toFrenchDateString(): string {
     const format: string = 'fr-FR';
@@ -143,7 +282,7 @@ export class ChrDate {
    * TODO: define what is a valid date
    */
   public isValid(): boolean {
-    let isValid: boolean =
+    const isValid: boolean =
       ChrDate._yearIsValid(this.year) &&
       ChrDate._monthIsValid(this.month) &&
       ChrDate._dayIsValid(this.day, this.month, this.year);
@@ -151,137 +290,8 @@ export class ChrDate {
     return isValid;
   }
 
-  /*******************************************
-   * Static methods
-   *******************************************/
-
-  /**
-   * Parse string from iso date String 'yyyy-mm-dd'
-   * @param isoDateString
-   * @returns ChrDate object. Is null if creation impossbile
-   */
-  public static createFromIsoString(isoDateString: string): ChrDate {
-    let date: ChrDate = null;
-    if (isoDateString) {
-      try {
-        let jsDate = new Date(isoDateString);
-        date = ChrDate.createFromDayMonthYear(jsDate.getDate(), jsDate.getMonth() + 1, jsDate.getFullYear());
-      } catch (err) {
-        // FIXME log to logger when dispo
-        console.error(err);
-        date = null;
-      }
-    }
-    return date;
-  }
-
-  /**
-   * Parse string from french date String 'dd/mm/yyyy'
-   * @param isoDateString
-   */
-  public static createFromFrenchDateString(dateString: string): ChrDate {
-    let date: ChrDate = null;
-    if (dateString) {
-      try {
-        dayjs.locale('fr');
-        const parseStrict = true;
-        const djsDate = dayjs(dateString, ['DD/MM/YYYY', 'D/M/YYYY', 'D/M/YY'], 'fr', parseStrict);
-        if (djsDate.isValid()) {
-          date = ChrDate.createFromDayMonthYear(djsDate.date(), djsDate.month() + 1, djsDate.year());
-        }
-      } catch (err) {
-        // FIXME log to logger when dispo
-        console.error(err);
-        date = null;
-      }
-    }
-    return date;
-  }
-
-  /**
-   * This creates only valid dates
-   * @param year number of the year
-   * @param month number of month (1 based not 0 like in js)
-   * @param day number of day
-   */
-  public static createFromDayMonthYear(day: number, month: number, year: number): ChrDate {
-    let date: ChrDate = null;
-    if (ChrDate._yearIsValid(year) && ChrDate._monthIsValid(month) && ChrDate._dayIsValid(day, month, year)) {
-      date = new ChrDate(day, month, year);
-    }
-    return date;
-  }
-
-  /**
-   * compares two dates
-   * @param aDate
-   * @param bDate
-   * @returns
-   */
-  public static compare(aDate: ChrDate, bDate: ChrDate): number {
-    let result: number = 0;
-
-    if (aDate && bDate) {
-      const aDateIso = aDate.toIsoDateString();
-      const bDateIso = bDate.toIsoDateString();
-      if (aDateIso < bDateIso) {
-        result = -1;
-      } else if (aDateIso === bDateIso) {
-        result = 0;
-      } else {
-        result = 1;
-      }
-    }
-    return result;
-  }
-
-  /**
-   * Gets the number of days in a month, with leap years, from the date object.
-   * @param year: 0 < year <= 9999
-   * @param month: number,  1 is january, 12 is december
-   */
-  private static _getMaxDaysOfMonthInYear(month: number, year: number): number {
-    let result = null;
-    if (year && year <= 9999 && month && month <= 12)
-      try {
-        // this is interpreted as the last day of the previous month
-        // since months are 0 based
-        let date = new Date(year, month, 0);
-        result = date.getDate();
-      } catch (err) {
-        // FIXME log to logger when dispo
-        console.error(err);
-        result = null;
-      }
-
-    return result;
-  }
-
-  /**
-   * Years must be in the interval [minYear, maxYear]
-   * Validation function as pure function
-   * @param year
-   */
-  private static _yearIsValid(year: number): boolean {
-    return Times.minYear <= year && year <= Times.maxYear;
-  }
-
-  /**
-   * Month is valid, if it is in the interval [0, monthsInYear]
-   * @param month
-   */
-  private static _monthIsValid(month: number): boolean {
-    return 1 <= month && month <= Times.monthsInYear;
-  }
-
-  /**
-   * TODO: comment
-   * @param day
-   * @param month
-   * @param year
-   */
-  private static _dayIsValid(day: number, month: number, year: number): boolean {
-    const maxDays = ChrDate._getMaxDaysOfMonthInYear(month, year);
-    return 0 < day && day <= maxDays;
+  test(): void {
+    const toto = '';
+    console.log(toto);
   }
 }
