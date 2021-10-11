60b55d5cd46bba997cff85cdf9561deb2ce5af87
jenpet <jens-petzold@asys.fr>
2021-10-11 18:13:37 +0200

    added data service

--- projects/qzWorte/src/app/app.component.ts
+++ projects/qzWorte/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppDataService } from './services/app-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'qzWorte';
+
+  public topicList = [];
+
+  constructor(private dataService: AppDataService) {}
+
+  ngOnInit(): void {
+    this.topicList = this.dataService.topicList;
+  }
 }
