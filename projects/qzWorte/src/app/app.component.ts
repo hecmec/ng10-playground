7254ebcec55eea97ba8cf54dfee7a21070b80d5d
jenpet <jens-petzold@asys.fr>
2021-10-12 23:43:02 +0200

    sort topics

--- projects/qzWorte/src/app/app.component.ts
+++ projects/qzWorte/src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { AppDataService } from './services/app-data.service';
+import { AppDataService, Topic } from './services/app-data.service';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+export interface TopicExtended extends Topic {
+  safeIconSvg: SafeHtml;
+}
+
+enum TopicColumns {
+  level = 'level',
+  name = 'name',
+  default = 'default',
+}
 
 @Component({
   selector: 'app-root',
@@ -9,11 +20,55 @@ import { AppDataService } from './services/app-data.service';
 export class AppComponent implements OnInit {
   title = 'qzWorte';
 
-  public topicList = [];
+  private topicList: Topic[] = [];
+  public topicExtendedList: TopicExtended[] = [];
+  public topicExtendedListOrigin: TopicExtended[] = [];
 
-  constructor(private dataService: AppDataService) {}
+  public topicListOrder = {
+    level: 1,
+    name: 1,
+    default: 1,
+  };
+
+  constructor(private dataService: AppDataService, private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
-    this.topicList = this.dataService.topicList;
+    this.topicList = this.dataService.topicList.slice();
+    let topicExt: TopicExtended = null;
+
+    this.topicExtendedList = this.topicList.map((topic) => {
+      topicExt = topic as unknown as TopicExtended;
+      topicExt.safeIconSvg = this.getSanitizedHtml(topic.iconSvg);
+      return topicExt;
+    });
+    this.topicExtendedListOrigin = this.topicExtendedList.slice();
+  }
+
+  getSanitizedHtml(html: string): SafeHtml {
+    let result: SafeHtml = '';
+    if (html) {
+      result = this.sanitizer.bypassSecurityTrustHtml(html);
+    }
+    return result;
+  }
+
+  sortList(sortDimension: string): void {
+    if (sortDimension && sortDimension in this.topicListOrder) {
+      this.topicListOrder[sortDimension] = this.topicListOrder[sortDimension] * -1;
+
+      if (sortDimension === TopicColumns.level) {
+        this.topicExtendedList = this.topicExtendedList.sort((a: TopicExtended, b: TopicExtended) => {
+          return this.topicListOrder[sortDimension] * (a.level - b.level);
+        });
+      } else if (sortDimension === TopicColumns.name) {
+        this.topicExtendedList = this.topicExtendedList.sort((a: TopicExtended, b: TopicExtended) => {
+          return this.topicListOrder[sortDimension] * (a.name <= b.name ? -1 : 1);
+        });
+      } else if (sortDimension === TopicColumns.default) {
+        this.topicExtendedList = this.topicExtendedListOrigin.sort((a: TopicExtended, b: TopicExtended) => {
+          return this.topicListOrder[sortDimension] * (a.name <= b.name ? -1 : 1);
+        });
+      }
+    }
   }
 }
