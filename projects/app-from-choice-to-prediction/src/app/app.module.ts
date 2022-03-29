94f7ff806d0a20d08dcc3a56073485808c674ee2
jenpet <jens-petzold@asys.fr>
2022-03-29 09:33:48 +0200

    added components

--- projects/app-from-choice-to-prediction/src/app/app.module.ts
+++ projects/app-from-choice-to-prediction/src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataHelperService } from './data-helper.service';
+import { HomePageComponent } from './home-page/home-page.component';
+import { QuestionsPageComponent } from './questions-page/questions-page.component';
+import { ResultPageComponent } from './result-page/result-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
-  declarations: [AppComponent, DataHelperService],
+  declarations: [AppComponent, DataHelperService, HomePageComponent, QuestionsPageComponent, ResultPageComponent, AboutPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
